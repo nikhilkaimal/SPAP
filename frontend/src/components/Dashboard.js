dd72ad25b73276e8d2ecf0456f125bfb37e5b8f4
nikhilkaimal <nikhilkjr96@gmail.com>
2020-10-22 16:23:21 +0530

    added mothers education

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -20,6 +20,159 @@ export default class Dashboard extends Component {
 
     if (redirectToLogin) return <Redirect to="/" />;
 
-    return <div>Dashboard</div>;
+    return (
+      <div className="container">
+        <form
+          className="mt-5"
+          style={{
+            width: "52%",
+            border: "1px solid black",
+            margin: "0 auto",
+            padding: "1%",
+          }}
+        >
+          <div className="row mt-2">
+            <div class="col">
+              <label>Gender</label>
+            </div>
+            <div class="col">
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio1"
+                  value="male"
+                />
+                <label class="form-check-label" for="inlineRadio1">
+                  Male
+                </label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value="female"
+                />
+                <label class="form-check-label" for="inlineRadio2">
+                  Female
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-2">
+            <div class="col">
+              <label>Age</label>
+            </div>
+            <div class="col">
+              <input type="number" class="form-control" />
+            </div>
+          </div>
+          <div className="row mt-2">
+            <div class="col">
+              <label>Location</label>
+            </div>
+            <div class="col">
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio1"
+                  value="urban"
+                />
+                <label class="form-check-label" for="inlineRadio1">
+                  Urban
+                </label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value="rural"
+                />
+                <label class="form-check-label" for="inlineRadio2">
+                  Rural
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-2">
+            <div class="col">
+              <label>Family Size</label>
+            </div>
+            <div class="col">
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio1"
+                  value="<3"
+                />
+                <label class="form-check-label" for="inlineRadio1">
+                  Less than 3
+                </label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value=">3"
+                />
+                <label class="form-check-label" for="inlineRadio2">
+                  More than 3
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-2">
+            <div class="col">
+              <label>Parents Co-habitation Status</label>
+            </div>
+            <div class="col">
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio1"
+                  value="together"
+                />
+                <label class="form-check-label" for="inlineRadio1">
+                  Living Together
+                </label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value="apart"
+                />
+                <label class="form-check-label" for="inlineRadio2">
+                  Living Apart
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-2">
+            <div class="col">
+              <label>Mother's Education</label>
+            </div>
+            <div class="col">
+              <input type="number" class="form-control" />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
